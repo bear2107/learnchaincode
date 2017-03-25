8c0ff0fdec1889965aa0f6df0d5cfec444c9d28c
bear2107 <Piyush.joshi@iiitb.org>
2017-03-25 13:40:47 +0530

    Update crowd_fund_chaincode.go

--- crowd_fund_chaincode/crowd_fund_chaincode.go
+++ crowd_fund_chaincode/crowd_fund_chaincode.go
@@ -1,42 +1,52 @@
+package main
+
+import (
+        "errors"
+        "fmt"
+        "strconv"
+        "encoding/json"
+        "github.com/hyperledger/fabric/core/chaincode/shim"
+)
 
 // CrowdFundChaincode implementation
 type CrowdFundChaincode struct {
 }
+type Info struct {
 
+qrcode string
+count int
+
+
+}
 //
 // Init creates the state variable with name "account" and stores the value
 // from the incoming request into this variable. We now have a key/value pair
 // for account --> accountValue.
 //
 func (t *CrowdFundChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	// State variable "account"
-	var account string
-	// The value stored inside the state variable "account"
-	var accountValue int
-	// Any error to be reported back to the client
-	var err error
+        // State variable "account"
+        // The value stored inside the state variable "account"
+        
+        // Any error to be reported back to the client
+        var err error
 
-	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2.")
-	}
+        if len(args) != 2 {
+                return nil, errors.New("Incorrect number of arguments. Expecting 2.")
+        }
 
-	// Initialize the state variable name
-	account = args[0]
-	// Initialize the state variable value
-	accountValue, err = strconv.Atoi(args[1])
-	if err != nil {
-		return nil, errors.New("Expecting integer value for account initialization.")
-	}
+        information := Info{}
+        informationbyte, err := json.Marshal(information)
+     if err!=nil {
+                        return nil, err
+                }
+                err=stub.PutState("default",informationbyte)
+         if err!=nil {
+                        return nil, err
+                }
 
-	fmt.Printf("accountValue = %d\n", accountValue)
 
-	// Write the state to the ledger
-	err = stub.PutState(account, []byte(strconv.Itoa(accountValue)))
-	if err != nil {
-		return nil, err
-	}
 
-	return nil, nil
+        return nil, nil
 }
 
 //
@@ -45,47 +55,40 @@ func (t *CrowdFundChaincode) Init(stub shim.ChaincodeStubInterface, function str
 // updating the ledger.
 //
 func (t *CrowdFundChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	// State variable "account"
-	var account string
-	// The value stored inside the state variable "account"
-	var accountValue int
-	// The ammount by which to increase the state variable
-	var increaseBy int
-	// Any error to be reported back to the client
-	var err error
+        // State variable "account"
+ var account string
+        // The value stored inside the state variable "account"
+//      var accountValue int
+        // The ammount by which to increase the state variable
+//      var increaseBy int
+        // Any error to be reported back to the client
+        var err error
 
-	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2.")
-	}
+        var value int
 
-	// Read in the name of the state variable to be updated
-	account = args[0]
+        if len(args) != 3 {
+                return nil, errors.New("Incorrect number of arguments. Expecting 2.")
+        }
+                value, err = strconv.Atoi(args[2])
+        if err != nil {
+                return nil, errors.New("Invalid count value, expecting a integer value.")
+        }
+        // Read in the name of the state variable to be updated
+        account = args[0]
+        newinfo := Info{
+	qrcode: args[1],
+        count: value}
+        newinfobyte,err:=json.Marshal(newinfo)
+        if err!=nil {
+                return nil,err
+        }
 
-	// Get the current value of the state variable
-	accountValueBytes, err := stub.GetState(account)
-	if err != nil {
-		return nil, errors.New("Failed to get account state.")
-	}
-	if accountValueBytes == nil {
-		return nil, errors.New("Entity not found!")
-	}
-	accountValue, _ = strconv.Atoi(string(accountValueBytes))
+        err = stub.PutState(account, newinfobyte)
+        if err != nil {
+                return nil, err
+        }
 
-	// Update the "account" state variable
-	increaseBy, err = strconv.Atoi(args[1])
-	if err != nil {
-		return nil, errors.New("Invalid transaction amount, expecting a integer value.")
-	}
-	accountValue = accountValue + increaseBy
-	fmt.Printf("accountValue = %d\n", accountValue)
-
-	// Write the state back to the ledger
-	err = stub.PutState(account, []byte(strconv.Itoa(accountValue)))
-	if err != nil {
-		return nil, err
-	}
-
-	return nil, nil
+        return nil, nil
 }
 
 //
@@ -93,42 +96,48 @@ func (t *CrowdFundChaincode) Invoke(stub shim.ChaincodeStubInterface, function s
 // in the response.
 //
 func (t *CrowdFundChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if function != "query" {
-		return nil, errors.New("Invalid query function name. Expecting \"query\".")
-	}
+  if function != "query" {
+                return nil, errors.New("Invalid query function name. Expecting \"query\".")
+        }
 
-	// State variable "account"
-	var account string
-	// Any error to be reported back to the client
-	var err error
+        // State variable "account"
+        var account string
+        // Any error to be reported back to the client
+        var err error
 
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the state variable to query.")
-	}
+        if len(args) != 1 {
+                return nil, errors.New("Incorrect number of arguments. Expecting name of the state variable to query.")
+        }
 
-	// Read in the name of the state variable to be returned
-	account = args[0]
-
-	// Get the current value of the state variable
-	accountValueBytes, err := stub.GetState(account)
-	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + account + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-	if accountValueBytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + account + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	jsonResp := "{\"Name\":\"" + account + "\",\"Amount\":\"" + string(accountValueBytes) + "\"}"
-	fmt.Printf("Query Response:%s\n", jsonResp)
-	return accountValueBytes, nil
+        // Read in the name of the state variable to be returned
+        account = args[0]
+        information:=Info{}
+        // Get the current value of the state variable
+        accountValueBytes, err := stub.GetState(account)
+        if err != nil {
+                jsonResp := "{\"Error\":\"Failed to get state for " + account + "\"}"
+   		 return nil, errors.New(jsonResp)
+        }
+        if accountValueBytes == nil {
+                jsonResp := "{\"Error\":\"Nil amount for " + account + "\"}"
+                return nil, errors.New(jsonResp)
+        }
+        errUnmarshal:=json.Unmarshal(accountValueBytes,&information)
+        if errUnmarshal!=nil {
+                return nil,errUnmarshal
+        }
+        jsonResp := "{\"Name\":\"" + account + "\",\"qrcode\":\"" + information.qrcode + "\",\"count\":\"" + string(information.count) + "\"}"
+        fmt.Printf("Query Response:%s\n", jsonResp)
+        return accountValueBytes, nil
 }
 
 func main() {
-	err := shim.Start(new(CrowdFundChaincode))
+        err := shim.Start(new(CrowdFundChaincode))
 
-	if err != nil {
-		fmt.Printf("Error starting CrowdFundChaincode: %s", err)
-	}
+        if err != nil {
+                fmt.Printf("Error starting CrowdFundChaincode: %s", err)
+        }
 }
+
+
+
