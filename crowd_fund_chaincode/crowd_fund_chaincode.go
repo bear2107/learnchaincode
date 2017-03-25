8e0aef716b5fa13058b17d4a0b7d1b023307e9f6
bear2107 <Piyush.joshi@iiitb.org>
2017-03-25 15:06:08 +0530

    Update crowd_fund_chaincode.go

--- crowd_fund_chaincode/crowd_fund_chaincode.go
+++ crowd_fund_chaincode/crowd_fund_chaincode.go
@@ -122,12 +122,12 @@ func (t *CrowdFundChaincode) Query(stub shim.ChaincodeStubInterface, function st
                 jsonResp := "{\"Error\":\"Nil amount for " + account + "\"}"
                 return nil, errors.New(jsonResp)
         }
-        errUnmarshal:=json.Unmarshal(accountValueBytes,&information)
-        if errUnmarshal!=nil {
-                return nil,errUnmarshal
-        }
-        jsonResp := "{\"Name\":\"" + account + "\",\"qrcode\":\"" + information.qrcode + "\",\"count\":\"" + information.count + "\"}"
-        fmt.Printf("Query Response:%s\n", jsonResp)
+   //     errUnmarshal:=json.Unmarshal(accountValueBytes,&information)
+     //   if errUnmarshal!=nil {
+       //         return nil,errUnmarshal
+       // }
+        //jsonResp := "{\"Name\":\"" + account + "\",\"qrcode\":\"" + information.qrcode + "\",\"count\":\"" + information.count + "\"}"
+        //fmt.Printf("Query Response:%s\n", jsonResp)
         return accountValueBytes, nil
 }
 
